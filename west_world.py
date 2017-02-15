750ec973a8d0c87e491c9839de517d5b2d311685
holopleather <holopleather@gmail.com>
2017-02-15 17:28:31 -0500

    added new  docstrings to west_world.py

--- west_world.py
+++ west_world.py
@@ -1,10 +1,10 @@
 import time
-
 import states
 import items
 
 
 class BaseGameEntity:
+    """Defines the class which game object's that are characters fall into. """
     id = 0
 
     def __init__(self):
@@ -13,11 +13,12 @@ class BaseGameEntity:
 
 
 class Miner(BaseGameEntity):
-    """The Miner game object
-
-    """
-
+    """The Miner game object."""
     def __init__(self, name, current_state, location, gold_carried, gold_bank, thirst, fatigue, build, pickax):
+        """ This BaseGameEntitiy's __init__ the variables name, current state, current location,
+            amount of gold carried, amount of gold in the bank, thirst, fatigue, max amount of gold that can be carried,
+            whether or not miner is in jail, number of times miner has been to jail, whether ot not the miner has a pickaxe, and
+            what type of build a miner has (which determines his hitpoints and strength)."""
         super(Miner, self).__init__()
         self.name = name
         self.current_state = current_state
@@ -26,7 +27,6 @@ class Miner(BaseGameEntity):
         self.gold_bank = gold_bank
         self.thirst = thirst
         self.fatigue = fatigue
-        self.max_nuggets = 5
         self.status = 'free'
         self.counter_jail = 0
         self.max_nuggets = 7
@@ -42,35 +42,43 @@ class Miner(BaseGameEntity):
             self.strength = 7 + self.pickax.strength
 
     def update(self):
+        """This gives the miner +1 thirst and executes a state."""
         self.thirst += 1
         self.current_state.execute(self)
 
     def change_state(self, new_state):
+        """This exits the current state, changes the current state, and then enters the new state."""
         self.current_state.exit(self)
         self.current_state = new_state
         self.current_state.enter(self)
 
     def pockets_full(self):
+        """Checks if gold_carried is equal to max_nuggets. If this is true, then his pockets are full."""
         if self.gold_carried > self.max_nuggets:
             return True
         else:
             return False
 
     def thirsty(self):
+        """The miner is thirsty if his thirst exceeds 10."""
         if self.thirst > 10:
             return True
         else:
             return False
 
     def is_tired(self):
+        """The miner is tired if his fatigue exceeds 10."""
         if self.fatigue > 10:
             return True
         else:
             return False
 
 class Wife(BaseGameEntity):
-
+    """The Miner's Wife game object """
     def __init__(self, name, wife_state, location, fatigue, dishes_washed, shirts_ironed, cups_made):
+        """This BaseGameEntity's __init__ includes the variables name, current state, current location, fatigue,
+            whether or not the dishes have been washed, whether or not the shirts have been ironed, how many cups of
+            coffee she has made, and the max amount of coffee she can make."""
         super(Wife, self).__init__()
         self.name=name
         self.wife_state=wife_state
@@ -82,21 +90,25 @@ class Wife(BaseGameEntity):
         self.max_cups = 2
 
     def update(self):
+        """This gives the wife +1 fatigue and executes a state."""
         self.fatigue+=1
         self.wife_state.execute(self)
 
     def wife_change_state(self, new_state):
+        """This exits the current state, changes the current state, and then enters the new state."""
         self.wife_state.exit(self)
         self.wife_state=new_state
         self.wife_state.enter(self)
 
     def tired(self):
+        """The wife is tired if her fatigue exceeds 10."""
         if self.fatigue > 4:
             return True
         else:
             return False
 
     def coffee_made(self):
+        """Checks if cups_made is equal to max_cups, and if this is true, the coffee has been made."""
         if self.cups_made == self.max_cups:
             return True
         else:
